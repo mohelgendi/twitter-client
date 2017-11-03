27a961484664b3c05268d76fc15cabffa2ff7de0
mohelgendi <mohelgendi@hotmail.com>
2017-11-03 19:35:24 +0300

    first commit

---
+++ src/testmongodb/TestMongoDB.java
@@ -0,0 +1,22 @@
+
+package testmongodb;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+public class TestMongoDB {
+
+    public static void main(String[] args) throws UnknownHostException{
+        
+     
+            mongodbForm form = new mongodbForm();
+            form.setVisible(true);
+            
+    }
+}
